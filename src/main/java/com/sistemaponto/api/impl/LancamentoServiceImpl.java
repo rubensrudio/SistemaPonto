b4bd396ae3217ba3a14639fd27b635e0d3884e21
Rubens Rudio <rubensrudio@gmail.com>
2020-08-04 10:18:00 -0300

    Add serviço de lançamento

---
+++ src/main/java/com/sistemaponto/api/impl/LancamentoServiceImpl.java
@@ -0,0 +1,48 @@
+package com.sistemaponto.api.impl;
+
+import java.util.Optional;
+
+import com.sistemaponto.api.models.Lancamento;
+import com.sistemaponto.api.repositories.LancamentoRepository;
+import com.sistemaponto.api.services.LancamentoService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService {
+
+    private static final Logger log = LoggerFactory.getLogger(LancamentoServiceImpl.class);
+
+    @Autowired
+    private LancamentoRepository lancamentoRepository;
+
+    @Override
+    public Page<Lancamento> buscarPorFuncionarioId(Long funcionarioId, PageRequest pageRequest) {
+        log.info("Buscando lançamentos para o funcionário ID {}", funcionarioId);
+        return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
+    }
+
+    @Override
+    public Optional<Lancamento> buscarPorId(Long id) {
+        log.info("Buscando lançamentos pelo ID {}", id);
+        return this.lancamentoRepository.findById(id);
+    }
+
+    @Override
+    public Lancamento persistir(Lancamento lancamento) {
+        log.info("Persistindo o lançamento {}", lancamento);
+        return this.lancamentoRepository.save(lancamento);
+    }
+
+    @Override
+    public void remover(Long id) {
+        log.info("Removendo o lançamento ID {}", id);
+        this.lancamentoRepository.deleteById(id);
+    }
+    
+}
